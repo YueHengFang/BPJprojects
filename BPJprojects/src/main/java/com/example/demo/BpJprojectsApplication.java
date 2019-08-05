67d4cf9ead7b3f118e2acdc9215c7a07947efcd9
“YueHengFang” <2836486971@qq.com>
2019-08-05 20:48:29 +0800

    second commit

--- BPJprojects/src/main/java/com/example/demo/BpJprojectsApplication.java
+++ BPJprojects/src/main/java/com/example/demo/BpJprojectsApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BpJprojectsApplication {
 
-//MacBook Pro
 	public static void main(String[] args) {
 		SpringApplication.run(BpJprojectsApplication.class, args);
 	}
